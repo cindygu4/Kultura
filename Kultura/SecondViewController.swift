e0c277807f7ae80db312125af4d89111adc42485
Cindy <cindygu@umich.edu>
2019-10-12 09:26:07 -0400

    Initial Commit

---
+++ Kultura/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Kultura
+//
+//  Created by Cindy on 10/12/19.
+//  Copyright © 2019 Cindy Gu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
