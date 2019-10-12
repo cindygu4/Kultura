48754a13bab1aea46003fa117972056294dc2051
Cindy <cindygu@umich.edu>
2019-10-12 11:02:41 -0400

    login view controller started

--- Kultura/LoginViewController.swift
+++ Kultura/LoginViewController.swift
@@ -3,7 +3,7 @@
 //  Kultura
 //
 //  Created by Cindy on 10/12/19.
-//  Copyright © 2019 Cindy Gu. All rights reserved.
+//  Copyright © 2019 Kultura. All rights reserved.
 //
 
 import SwiftUI
@@ -24,6 +24,37 @@ class LoginViewController: UIViewController {
     @IBAction func loginTapped(_ sender: UIButton) {
         
         // get default auth ui object
+        let authUI = FUIAuth.defaultAuthUI()
+        
+        guard authUI != nil else {
+            // log error
+            return
+        }
+        
+        // set as delegate
+        authUI?.delegate = self
+        
+        // get reference to auth ui view controller
+        let authViewController = authUI!.authViewController()
+        
+        // show
+        present(authViewController, animated: true, completion: nil)
+        
     }
 }
 
+
+
+extension UIViewController: FUIAuthDelegate {
+    public func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        // check for errors
+        guard error == nil else {
+            // log the error
+            return
+        }
+        
+        // AuthDataResult?.user.uid
+        
+        performSegue(withIdentifier: "goHome", sender: self)
+    }
+}
