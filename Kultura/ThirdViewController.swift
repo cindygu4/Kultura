f492b4ac1cc082207e21d80fc5594479062307ba
Cindy <cindygu@umich.edu>
2019-10-12 10:21:01 -0400

    New commits

---
+++ Kultura/ThirdViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ThirdViewController.swift
+//  Kultura
+//
+//  Created by Cindy on 10/12/19.
+//  Copyright © 2019 Cindy Gu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ThirdViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThirdViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ThirdViewController()
+    }
+}
