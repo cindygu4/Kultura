b87d29e2d37ab34bc3624d72842d898d9fb0d966
Cindy <cindygu@umich.edu>
2019-10-12 10:22:19 -0400

    Workspace added

---
+++ Kultura/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  Kultura
+//
+//  Created by Cindy on 10/12/19.
+//  Copyright © 2019 Cindy Gu. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+import FirebaseUI
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        
+        // get default auth ui object
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+platform :ios, '13.1'
+
+use_frameworks!
+
+target "Kultura" do
+	pod 'Firebase/Core'
+	pod 'Firebase/Firestore'
+	pod 'FirebaseUI'
+	pod 'Firebase'
+	pod 'FirebaseUI/Google'
+	pod 'Firebase/Auth'
+	pod 'FirebaseUI/Auth'
+	pod 'GoogleSignIn'
+	pod 'GoogleMaps'
+end
