52d6627dc2453d7d8e5c24a5011bd71813b9ce5a
Cindy <cindygu@umich.edu>
2019-10-12 10:24:43 -0400

    added pods

---
+++ Kultura/Colors/Colors.swift
@@ -0,0 +1,22 @@
+//
+//  Colors.swift
+//  Kultura
+//
+//  Created by Cindy Gu on 10/12/19.
+//
+
+import Foundation
+import UIKit
+
+public struct Colors {
+    static let yellow = UIColor(red: 255, green: 193, blue: 94, alpha: 0.0)
+    static let coral = UIColor(red: 255, green: 131, blue: 96, alpha: 0.0)
+    static let lightBlue = UIColor(red: 146, green: 229, blue: 222, alpha: 0.0)
+    static let iguanaGreen = UIColor(red: 120, green: 201, blue: 136, alpha: 0.0)
+    // pearl is the beigy color that looks nice
+    static let pearl = UIColor(red: 234, green: 224, blue: 204, alpha: 0.0)
+    // TODO: add startBackgroundGradient
+    // TODO: add endBackgroundGradient
+    // TODO: add tab colors
+    // TODO: top bar color
+}
