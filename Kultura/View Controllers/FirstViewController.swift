b5d1d796a6b9567013f93a7bd03fda04375c7ac9
jazzyw <jazzyw@umich.edu>
2019-10-13 10:11:35 -0400

    idk

--- Kultura/View Controllers/FirstViewController.swift
+++ Kultura/View Controllers/FirstViewController.swift
@@ -14,6 +14,7 @@ struct Headline {
     var title : String
     var text : String
     var image : String
+    var code : Int
 }
 
 class FirstViewController: UITableViewController {
@@ -21,12 +22,13 @@ class FirstViewController: UITableViewController {
     /*override func numberOfSections(in tableView: UITableView) -> Int {
         return 3;
     }*/
+    let randomInt = Int.random(in: 10000..<99999)
     
-    var headlines = [Headline(id: 1, title: "Celebrasia", text: "Chinese Student Association", image: "Celebrate"),
-                     Headline(id: 2, title: "Skate Night", text: "TASA", image: "IceSkate"),
-                     Headline(id: 3, title: "Lunar Gala", text: "Asian American Student Association", image: "What"),
-                     Headline(id: 4, title: "Taste of India", text: "Indian Student Association", image: "TasteOfIndia"),
-                     Headline(id: 5, title: "Congee Night", text: "Chinese Student Association", image: "Congee")]
+    var headlines = [Headline(id: 1, title: "Celebrasia", text: "Chinese Student Association", image: "Celebrate", code: Int.random(in:10000..<99999)),
+                     Headline(id: 2, title: "Skate Night", text: "TASA", image: "IceSkate", code: Int.random(in:10000..<99999)),
+                     Headline(id: 3, title: "Lunar Gala", text: "Asian American Student Association", image: "What", code: Int.random(in:10000..<99999)),
+                     Headline(id: 4, title: "Taste of India", text: "Indian Student Association", image: "TasteOfIndia", code: Int.random(in:10000..<99999)),
+                     Headline(id: 5, title: "Congee Night", text: "Chinese Student Association", image: "Congee", code: Int.random(in:10000..<99999))]
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return headlines.count;
@@ -39,8 +41,13 @@ class FirstViewController: UITableViewController {
         cell.textLabel?.text = headline.title
         cell.detailTextLabel?.text = headline.text
         cell.imageView?.image = UIImage(named:headline.image)
+        cell.int = headline.code
+        
         return cell
     }
     
+    @IBAction func CodeClicked(_ sender: Any) {
+        
+    }
 }
 
