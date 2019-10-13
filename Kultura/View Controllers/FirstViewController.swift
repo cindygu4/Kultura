79decdfd05618f6cceb5384c5990985f69b1f8b0
Cindy <cindygu@umich.edu>
2019-10-13 10:33:38 -0400

    added some events

--- Kultura/View Controllers/FirstViewController.swift
+++ Kultura/View Controllers/FirstViewController.swift
@@ -17,35 +17,15 @@ struct Headline {
     var code : Int
 }
 
-class FirstViewController: UITableViewController {
+class FirstViewController: UIViewController {
     
-    /*override func numberOfSections(in tableView: UITableView) -> Int {
-        return 3;
-    }*/
-    let randomInt = Int.random(in: 10000..<99999)
-    
-    var headlines = [Headline(id: 1, title: "Celebrasia", text: "Chinese Student Association", image: "Celebrate", code: Int.random(in:10000..<99999)),
-                     Headline(id: 2, title: "Skate Night", text: "TASA", image: "IceSkate", code: Int.random(in:10000..<99999)),
-                     Headline(id: 3, title: "Lunar Gala", text: "Asian American Student Association", image: "What", code: Int.random(in:10000..<99999)),
-                     Headline(id: 4, title: "Taste of India", text: "Indian Student Association", image: "TasteOfIndia", code: Int.random(in:10000..<99999)),
-                     Headline(id: 5, title: "Congee Night", text: "Chinese Student Association", image: "Congee", code: Int.random(in:10000..<99999))]
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return headlines.count;
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath)
-        
-        let headline = headlines[indexPath.row]
-        cell.textLabel?.text = headline.title
-        cell.detailTextLabel?.text = headline.text
-        cell.imageView?.image = UIImage(named:headline.image)
-        cell.int = headline.code
-        
-        return cell
-    }
-    
+  
     @IBAction func CodeClicked(_ sender: Any) {
         
     }
